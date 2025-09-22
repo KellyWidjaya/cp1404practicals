62bc03cad34a2e3ab75dc51ebe9c51961d28d99e
KellyWidjaya <kelly.widjaya@my.jcu.edu.au>
2025-09-22 09:25:30 +0800

    Refactor password check program to use functions

--- prac_01/broken_score.py
+++ prac_01/broken_score.py
@@ -3,35 +3,41 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 Pseudocode:
 
-VALID_THRESHOLD = 100
+MINIMUM_SCORE = 0
+MAXIMUM_SCORE = 100
 EXCELLENT_THRESHOLD = 90
-PASSABLE_THRESHOLD = 50
+PASS_THRESHOLD = 50
 
 get score
-if score < 0 or score > VALID_THRESHOLD
-    result = "Invalid score"
-else if score >= EXCELLENT_THRESHOLD
-    result = "Excellent"
-else if score >= PASSABLE_THRESHOLD
-    result = "Passable"
+if score < MINIMUM_SCORE or score > MAXIMUM_SCORE
+    message = "Invalid score"
 else
-    result = "Bad"
-print result
+    if score >= EXCELLENT_THRESHOLD
+        message = "Excellent"
+    else if score >= PASS_THRESHOLD
+        message = "Passable"
+    else
+        message = "Bad"
+
+print message
 """
 
 # TODO: Fix this!
 
-VALID_THRESHOLD = 100
+MINIMUM_SCORE = 0
+MAXIMUM_SCORE = 100
 EXCELLENT_THRESHOLD = 90
-PASSABLE_THRESHOLD = 50
+PASS_THRESHOLD = 50
 
 score = float(input("Enter score: "))
-if score < 0 or score > VALID_THRESHOLD:
-    result = "Invalid score"
-elif score >= EXCELLENT_THRESHOLD:
-    result = "Excellent"
-elif score >= PASSABLE_THRESHOLD:
-    result = "Passable"
+if score < MINIMUM_SCORE or score > MAXIMUM_SCORE:
+    message = "Invalid score"
 else:
-    result = "Bad"
-print(result)
+    if score >= EXCELLENT_THRESHOLD:
+        message = "Excellent"
+    elif score >= PASS_THRESHOLD:
+        message = "Passable"
+    else:
+        message = "Bad"
+
+print(message)
