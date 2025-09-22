9cf4d49f2e8d413228fc7134561257b774805231
KellyWidjaya <kelly.widjaya@my.jcu.edu.au>
2025-09-22 14:35:55 +0800

    Refactor score status program to use functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,23 +2,29 @@
 CP1404/CP5632 - Practical
 Program to determine score status
 """
+
 import random
+
+MINIMUM_SCORE = 0
+MAXIMUM_SCORE = 100
+EXCELLENT_THRESHOLD = 90
+PASS_THRESHOLD = 50
 
 def main():
     score = float(input("Enter score: "))
     score_result = determine_result(score)
-    random_score = random.randint(1, 100)
+    random_score = random.randint(MINIMUM_SCORE, MAXIMUM_SCORE)
     random_result = determine_result(random_score)
     print(score_result)
     print(f"Random score: {random_score}")
     print(random_result)
 
 def determine_result(score):
-    if score < 0 or score > 100:
+    if score < MINIMUM_SCORE or score > MAXIMUM_SCORE:
         return "Invalid score"
-    elif score >= 90:
+    elif score >= EXCELLENT_THRESHOLD:
         return "Excellent"
-    elif score >= 50:
+    elif score >= PASS_THRESHOLD:
         return "Passable"
     else:
         return "Bad"
