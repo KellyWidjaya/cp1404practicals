12433d3510367a15a8e55af30315dae05c446050
KellyWidjaya <kelly.widjaya@my.jcu.edu.au>
2025-10-05 11:42:16 +0800

    Add code to sum first two numbers from numbers.txt

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,9 +1,19 @@
+# 1.
 name = input("Enter your name: ")
 out_file = open("name.txt", "w")
 out_file.write(name)
 out_file.close()
 
+# 2.
 in_file = open("name.txt", "r")
 name = in_file.read()
 in_file.close()
 print(f"Hi {name}!")
+
+# 3.
+with open("numbers.txt", "r") as in_file:
+    first_number = int(in_file.readline())   # Read the first line and convert to int
+    second_number = int(in_file.readline())  # Read the second line and convert to int
+
+result = first_number + second_number
+print(f"Result: {result}")
