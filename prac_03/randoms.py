df1df11f6d2e9fa1d7495130efbd2765300e4dd1
KellyWidjaya <kelly.widjaya@my.jcu.edu.au>
2025-10-05 09:46:49 +0800

    Add question answers on random functions output

---
+++ prac_03/randoms.py
@@ -0,0 +1,18 @@
+# What did you see on line 1?
+# I saw 13 on line 1.
+# What was the smallest number you could have seen, what was the largest?
+# The smallest number I could have seen was 5, and the largest was 20.
+
+# What did you see on line 2?
+# I saw 5 on line 2.
+# What was the smallest number you could have seen, what was the largest?
+# The smallest number I could have seen was 3, and the largest was 9.
+# Could line 2 have produced a 4?
+# No, because the step is 2 (it only produces odd numbers between 3 and 9).
+
+# What did you see on line 3?
+# I saw 3.870037799430254 on line 3.
+# What was the smallest number you could have seen, what was the largest?
+# The smallest number I could have seen was 2.5, and the largest was 5.5.
+
+# Write code, not a comment, to produce a random number between 1 and 100 inclusive.
