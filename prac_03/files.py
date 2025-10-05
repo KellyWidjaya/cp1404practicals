bca9689ca4ff97b8e3111cc50ec16ff3fbe0b85e
KellyWidjaya <kelly.widjaya@my.jcu.edu.au>
2025-10-05 11:34:53 +0800

    Add code to read name.txt and greet the user

--- prac_03/files.py
+++ prac_03/files.py
@@ -2,3 +2,8 @@ name = input("Enter your name: ")
 out_file = open("name.txt", "w")
 out_file.write(name)
 out_file.close()
+
+in_file = open("name.txt", "r")
+name = in_file.read()
+in_file.close()
+print(f"Hi {name}!")
