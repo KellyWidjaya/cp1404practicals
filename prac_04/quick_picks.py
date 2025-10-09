e38e20246b4b5f321c7693947adf36235a9597c1
KellyWidjaya <kelly.widjaya@my.jcu.edu.au>
2025-10-09 20:08:38 +0800

    Add quick picks number generator program

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,20 @@
+"""
+CP1404/CP5632 Practical
+"Quick Pick" Lottery Ticket Generator
+"""
+
+import random
+
+NUMBERS_PER_LINE = 6
+MINIMUM_NUMBER = 1
+MAXIMUM_NUMBER = 45
+
+number_of_picks = int(input("How many quick picks? "))
+
+number_width = len(str(MAXIMUM_NUMBER))
+for i in range(number_of_picks):
+    numbers = []
+    for j in range(NUMBERS_PER_LINE):
+        number = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
+        numbers.append(number)
+    print(" ".join(f"{number}" for number in numbers))
