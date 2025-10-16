85e9db34c3af344e6bf4a953ae63134a53b3200f
KellyWidjaya <kelly.widjaya@my.jcu.edu.au>
2025-10-16 09:23:45 +0800

    Add loop that prints all the states and names with string formatting

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -9,6 +9,11 @@ CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern T
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
 print(CODE_TO_NAME)
 
+code_width = max(len(code) for code in CODE_TO_NAME.keys())
+
+for code, name in CODE_TO_NAME.items():
+    print(f"{code:{code_width}} is {name}")
+
 state_code = input("Enter short state: ").upper()
 while state_code != "":
     if state_code in CODE_TO_NAME:
