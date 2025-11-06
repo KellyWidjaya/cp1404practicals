080187a0ccbec19baa413c7ff671dbda67a7c450
KellyWidjaya <kelly.widjaya@my.jcu.edu.au>
2025-11-06 11:05:54 +0800

    Update myguitars.py to add new guitars and save to guitars.csv

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -9,11 +9,9 @@ FILENAME = 'guitars.csv'
 def main():
     """Load and display guitars."""
     guitars = load_guitars()
-
-    guitars.sort()
-    for guitar in guitars:
-        print(guitar)
-
+    add_guitars(guitars)
+    save_guitars(guitars)
+    display_guitars(guitars)
 
 def load_guitars():
     """Read guitars from file into a list of Guitar objects."""
@@ -27,5 +25,33 @@ def load_guitars():
     in_file.close()
 
     return guitars
+
+def add_guitars(guitars):
+    """Get new guitars and add them to the list."""
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+
+        print(f"{guitar} added.")
+        print()
+
+        name = input("Name: ")
+
+def save_guitars(guitars):
+    """Save all guitars to the CSV file."""
+    out_file = open(FILENAME, 'w', encoding='utf-8')
+    for guitar in guitars:
+        print(f"{guitar.name},{guitar.year},{guitar.cost}", file=out_file)
+    out_file.close()
+
+def display_guitars(guitars):
+    """Print list of guitars neatly."""
+    guitars.sort()
+    for guitar in guitars:
+        print(guitar)
 
 main()
