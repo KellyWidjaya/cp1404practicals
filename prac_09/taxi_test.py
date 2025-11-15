7a5a7b0b48d48f3836d336b800f9bc01fadf8848
KellyWidjaya <kelly.widjaya@my.jcu.edu.au>
2025-11-15 22:19:15 +0800

    Update taxi_test.py to match new Taxi class

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -7,7 +7,7 @@ from prac_09.taxi import Taxi
 
 def main():
     """Test the Taxi class methods."""
-    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi = Taxi("Prius 1", 100)
 
     my_taxi.drive(40)
     print(my_taxi)
