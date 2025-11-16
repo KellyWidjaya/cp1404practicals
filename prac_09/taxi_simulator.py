888fe1950f933e7e647ddcc81087aaf2a7cfeb3c
KellyWidjaya <kelly.widjaya@my.jcu.edu.au>
2025-11-16 09:54:41 +0800

    Update taxi_simulator.py with taxi selection and display function

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -10,18 +10,36 @@ MENU = "q)uit, c)hoose taxi, d)rive"
 
 def main():
     """Taxi simulator menu program that uses Taxi and SilverServiceTaxi classes."""
+    total_bill = 0.0
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    current_taxi = None
+
     print("Let's drive!")
     print(MENU)
-    choice = input(">>> ").lower()
-    while choice != "q":
-        if choice == "c":
-            print("choose taxi")
-        elif choice == "d":
+    menu_choice = input(">>> ").lower()
+    while menu_choice != "q":
+        if menu_choice == "c":
+            print("Taxis available: ")
+            display_taxis(taxis)
+
+            try:
+                taxi_choice = int(input("Choose taxi: "))
+                current_taxi = taxis[taxi_choice]
+            except (ValueError, IndexError):
+                print("Invalid taxi choice")
+        elif menu_choice == "d":
             print("drive")
         else:
             print("Invalid option")
+
+        print(f"Bill to date: ${total_bill:.2f}")
         print(MENU)
-        choice = input(">>> ").lower()
+        menu_choice = input(">>> ").lower()
     print("quit")
+
+def display_taxis(taxis):
+    """Print list of taxis."""
+    for i, taxi in enumerate(taxis):
+        print(f"{i} - {taxi}")
 
 main()
