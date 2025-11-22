1aef6fd8d5b82b864cc689bf2d669f5f06c35f93
KellyWidjaya <kelly.widjaya@my.jcu.edu.au>
2025-11-22 16:08:15 +0800

    Update flask app with modifications

--- prac_10/app.py
+++ prac_10/app.py
@@ -5,7 +5,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return '<h1>Hello World :)</h1>'
+
+@app.route('/greet')
+@app.route('/greet/<name>')
+def greet(name=""):
+    return f"Hello {name}"
+
+def convert_celsius_to_fahrenheit(celsius):
+    """Calculate fahrenheit from celsius value"""
+    return (celsius * 9/5) + 32
+
+@app.route('/f/<celsius>')
+def display_fahrenheit(celsius):
+    fahrenheit = convert_celsius_to_fahrenheit(float(celsius))
+    return f"{celsius}°C is {fahrenheit:.2f}°F"
 
 
 if __name__ == '__main__':
